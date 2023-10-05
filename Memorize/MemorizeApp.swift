94242fea2ff3ad11a086c3abe89284a94a03ffb7
ubeyt <ubeytdemir4se@gmail.com>
2023-10-05 23:30:12 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ubeyt Demir on 5.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
